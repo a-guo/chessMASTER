6ea93364609debdf44e4d15941487fcdd5bf52ef
Addie Guo <haoshu.guo@mapleleaf.com>
2018-01-20 17:31:27 -0500

    Changed location of image files

--- chessMASTER/main.cpp
+++ chessMASTER/main.cpp
@@ -1,32 +1,51 @@
-//
-//  main.cpp
-//  chessMASTER
-//
-//  Created by Addie Guo on 2018-01-19.
-//  Copyright © 2018 Addie Guo. All rights reserved.
-//
-
 #include <SFML/Graphics.hpp>
+#include <time.h>
 
 using namespace sf;
 
 int main() {
-    RenderWindow window (VideoMode (640, 480), "Awesome Game");
+    RenderWindow window (VideoMode (453, 453), "chassMASTER");
+    
+    Texture t1;
+    t1.loadFromFile("images/figures.png");
+    
+    Sprite s(t1);
+    
+    bool isMove = false;
+    float dx = 0;
+    float dy = 0;
     
     while (window.isOpen()) {
-        Event event;
+        Vector2i pos = Mouse::getPosition(window);
         
-        while (window.pollEvent(event)) {
-            switch(event.type) {
-                case Event::Closed:
-                    window.close();
-                    
-                    break;
-            }
+        
+        Event e;
+        while (window.pollEvent(e)) {
+            if (e.type == Event::Closed)
+                window.close();
+            
+            //drag & drop//
+            if (e.type == Event::MouseButtonPressed)
+                if (e.key.code == Mouse::Left)
+                    if (s.getGlobalBounds().contains(pos.x, pos.y))
+                    {
+                        isMove = true;
+                        dx = pos.x - s.getPosition().x;
+                        dy = pos.y - s.getPosition().y;
+                    }
+            if (e.type == Event::MouseButtonPressed)
+                if (e.key.code == Mouse::Left)
+                    isMove = false;
+            
         }
         
-        window.clear();
-        window.display();
+        if (isMove) s.setPosition(pos.x - dx, pos.y - dy);
         
+        // draw //
+        window.clear();
+        window.draw(s);
+        window.display();
     }
+    
+    return 0;
 }
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+
+using namespace sf;
+
+int main() {
+    RenderWindow window (VideoMode (453, 453), "chassMASTER");
+    
+    Texture t1;
+    t1.loadFromFile("images/figures.png");
+
+    Sprite s(t1);
+
+    bool isMove = false;
+    float dx = 0; dy = 0;
+
+    while (window.isOpen()) {
+        Vector2i pos = Mouse::getPosition(window);
+
+
+        Event e;
+        while (window.pollEvent(e)) {
+        	if (e.type == Event::Closed) 
+        		window.close();
+
+        	//drag & drop//
+        	if (e.type == Event::MouseButtonPressed)
+        		if (e.key.code == Mouse::Left)
+        			if (s.getGlobalBounds().contains(pos.x, pos.y))
+        			{
+        				isMove = true;
+        				dx = pos.x - s.getPosition().x;
+        				dy = pos.y - s.getPosition().y;
+        			}
+        	if (e.type == Event::MouseButtonPressed)
+        		if (e.key.code == Mouse::Left)
+        			isMove = false;
+        	
+        }
+
+        if (isMove) s.setPostion(pos.x - dx, pos.y - dy);
+
+    // draw //
+    window.clear();
+    window.draw(s);
+    window.display();
+    }
+
+    return 0;
+}
