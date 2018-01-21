639b9003f1298a1b9187bcb02f4809e5c8f27b5f
Addie Guo <addieguo98@gmail.com>
2018-01-21 18:19:36 -0500

    Add undo option to main.cpp

--- chessMASTER/main.cpp
+++ chessMASTER/main.cpp
@@ -19,6 +19,32 @@ int board[8][8] =
     1,  2,  3,  4,  5,  3,  2,  1,
 };
 
+std::string toChessNote(Vector2f p) {
+    std::string s = "";
+    s += char(p.x/size + 97);
+    s += char(7 - p.y/size + 49);
+    return s;
+}
+
+Vector2f toCoord(char a, char b) {
+    int x = int(a) - 97;
+    int y = 7 - int(b) + 49;
+    return Vector2f(x * size, y * size);
+}
+
+void move(std::string str) {
+    Vector2f oldPos = toCoord(str[0], str[1]);
+    Vector2f newPos = toCoord(str[2], str[3]);
+    
+    for (int i = 0; i < 32; i++)
+        if (f[i].getPosition() == newPos) f[i].setPosition(-100, -100);
+    
+    for (int i = 0; i < 32; i++)
+        if (f[i].getPosition() == oldPos) f[i].setPosition(newPos);
+}
+
+std::string position = "";
+
 void loadPosition() {
     int k = 0;
     for (int i = 0; i < 8; i ++)
@@ -31,6 +57,8 @@ void loadPosition() {
             f[k].setPosition(size * j, size * i);
             k++;
         }
+    for (int i = 0; i < position.length(); i += 5)
+        move(position.substr(i, 4));
 }
 
 int main() {
@@ -47,8 +75,11 @@ int main() {
     
     loadPosition();
     
+    
     bool isMove = false;
     float dx = 0, dy = 0;
+    Vector2f oldPos, newPos;
+    std::string str;
     int n = 0;
     
     while (window.isOpen()) {
@@ -59,7 +90,16 @@ int main() {
             if (e.type == Event::Closed)
                 window.close();
             
-            //drag & drop//
+            // move back //
+            if (e.type == Event::KeyPressed)
+                if (e.key.code == Keyboard::BackSpace)
+                {
+                    position.erase(position.length() - 6, 5);
+                    loadPosition();
+                }
+            
+            
+            // drag & drop //
             if (e.type == Event::MouseButtonPressed)
                 if (e.mouseButton.button == Mouse::Left)
                     for (int i = 0; i < 32; i++)
@@ -69,6 +109,7 @@ int main() {
                             n = i;
                             dx = pos.x - f[i].getPosition().x;
                             dy = pos.y - f[i].getPosition().y;
+                            oldPos = f[i].getPosition();
                         }
             if (e.type == Event::MouseButtonReleased)
                 if (e.mouseButton.button == Mouse::Left)
@@ -76,9 +117,38 @@ int main() {
                     isMove = false;
                     Vector2f p = f[n].getPosition() + Vector2f(size/2, size/2);
                     Vector2f newPos = Vector2f(size * int(p.x/size), size * int(p.y/size));
+                    str = toChessNote(oldPos) + toChessNote(newPos);
+                    move(str);
+                    position += str + " ";
+                    std::cout << str << std::endl;
                     f[n].setPosition(newPos);
                 }
         }
+        
+        // comp move //
+        
+        if (Keyboard::isKeyPressed(Keyboard::Space)) {
+            str = "d7d5"; // for example
+            
+            oldPos = toCoord(str[0], str[1]);
+            newPos = toCoord(str[2], str[3]);
+            
+            for (int i = 0; i < 32; i++) if (f[i].getPosition() == oldPos) n = i;
+            
+            // animation //
+            for (int k = 0; k < 50; k ++) {
+                Vector2f p = newPos - oldPos;
+                f[n].move(p.x/50, p.y/50);
+                window.draw(sBoard);
+                for (int i = 0; i < 32; i++) window.draw(f[i]) ; window.draw(f[n]);
+                window.display();
+            }
+            
+            move(str);
+            position += str + " ";
+            f[n].setPosition(newPos);
+        }
+        
         
         if (isMove) f[n].setPosition(pos.x - dx, pos.y - dy);
         
